f3f9328c922de06585035c96dc7c0697856c1f1a
Helen <paznachka@gmail.com>
2021-02-04 20:34:13 +0200

    added component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// const el = React.createElement(
+//   "div",
+//   {
+//     id: "test"
+//   },
+//   "Hello world"
+// )
 
-const el = React.createElement(
-  "h1",
-  null,
-  "Hello world"
-)
+const Hi = () => {
+    return (
+        <h1>Hello world</h1>
+    )
+}
+const el = <div>
+    <Hi/>
+    <p>lorem</p>
+</div>
+
+
 
 ReactDOM.render(el,document.getElementById("root"))
