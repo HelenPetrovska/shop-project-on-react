b21528a5db1f9b423723bd9701ff5aa7c604feae
Helen <paznachka@gmail.com>
2021-02-09 20:55:10 +0200

    added Logo, Menu, Cart

---
+++ src/App/Header/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div className="logo text-center">
+            <img src="img/logo.png" alt=""/>
+        </div>
+    )
+}
+
+export default Logo
