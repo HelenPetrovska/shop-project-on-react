cb606cf0a7fca1efd92e868fdc4892bfac2692d8
Helen <paznachka@gmail.com>
2021-02-09 19:38:55 +0200

    added basic application structure

--- src/index.js
+++ src/index.js
@@ -1,49 +1,58 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import "./reset.css"
+import "./base.css"
+import "./header.css"
+import "./menu.css"
+import "./cart.css"
 
-// const el = React.createElement(
-//   "div",
-//   {
-//     id: "test"
-//   },
-//   "Hello world"
-// )
-
-const Header = () => {
-    return (
-        <h1>Hello world</h1>
-    )
-}
-
-const List = () => {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-        </ul>
-    )
-}
-
-const P = () => {
-    return (
-        <>
-            <p>lorem1</p>
-            <p>lorem2</p>
-            <p>lorem3</p>
-        </>
-    )
-}
 
 const App = () => {
     return (
         <>
-            <List/>
-            <P/>
-            <Header/>
+            <header class="header">
+		        <div class="container">
+                    <div class="header-row">
+                        <div class="logo text-center">
+                            <img src="img/logo.png" alt=""/>
+                        </div>
+                        
+                        <div class="menu">
+                            <ul>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Products</a></li>
+                                <li><a href="">Payment</a></li>
+                                <li><a href="">Shipping</a></li>
+                            </ul>
+                        </div>
+                        
+                        <div class="cart text-center">
+                            <div class="products-count">0</div>
+                            <div class="products-price">$500</div>
+                        </div>
+                    </div>
+		        </div>
+	        </header>
+            <main class="main">
+                <div class="container">
+                    <div class="row">
+                        <div class="aside">
+                            Filter
+                        </div>
+                        <div class="main-content">
+                            Product List
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+                        Footer
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
