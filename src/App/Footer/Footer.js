00ed8ed842cc3c8104ffb1e6612caa9b89391f73
Helen <paznachka@gmail.com>
2021-02-09 20:32:45 +0200

    built the structure

---
+++ src/App/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    Footer
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
