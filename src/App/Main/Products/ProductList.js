9e9d654974591480194e1197475970b774f4e418
Helen <paznachka@gmail.com>
2021-02-11 20:53:57 +0200

    added destructure to products.map

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -1,39 +1,33 @@
 import React from 'react'
 import ProductListItem from './ProductListItem'
+import products from './products'
 
 const ProductList = () => {
     return (
         <>
             <h1 className="page-title">Product List</h1>
             <div className="product-list">
-                <ProductListItem
-                    name="iPhone X"
-                    description="This is phone"
-                    type="phone"
-                    cepacity="1"
-                    price="1000"
-                />
-                <ProductListItem
-                    name="iPhone XS"
-                    description="This is phone"
-                    type="phone"
-                    cepacity="2"
-                    price="2000"
-                />
-                <ProductListItem
-                    name="iPhone 12"
-                    description="This is phone"
-                    type="phone"
-                    cepacity="1"
-                    price="1000"
-                />
-                <ProductListItem
-                    name="iPhone 8 plus"
-                    description="This is phone"
-                    type="phone"
-                    cepacity="2"
-                    price="2000"
-                />
+                {
+                    products.map(({
+                        id,
+                        name,
+                        description,
+                        price,
+                        cepacity,
+                        type
+                    }) => {
+                        return (                
+                            <ProductListItem
+                                key={id}
+                                name={name}
+                                description={description}
+                                type={type}
+                                cepacity={cepacity}
+                                price={price}
+                            />
+                        )
+                    })
+                }
             </div>
         </>
     )
---
+++ src/App/Main/Products/products.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+        id:1,
+        name:"iPhone X",
+        description:"This is iphone X",
+        type:"phone",
+        cepacity:"64",
+        price:"1000",
+    },
+    {
+        id:2,
+        name:"iPhone XS",
+        description:"This is iphone XS",
+        type:"phone",
+        cepacity:"256",
+        price:"1500",
+    },
+    {
+        id:3,
+        name:"iPhone 12",
+        description:"This is iphone 12",
+        type:"phone",
+        cepacity:"128",
+        price:"2000",
+    },
+    {
+        id:4,
+        name:"iPhone 8 plus",
+        description:"This is iphone 8 plus",
+        type:"phone",
+        cepacity:"64",
+        price:"1000",
+    },
+]
+
+export default products
