4972c593406bbc2ea598395a2e6af3bbfad3b545
Helen <paznachka@gmail.com>
2021-02-11 20:14:01 +0200

    added destructure

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -5,34 +5,36 @@ const ProductList = () => {
     return (
         <>
             <h1 className="page-title">Product List</h1>
-            <ProductListItem
-                name="iPhone X"
-                description="This is phone"
-                type="phone"
-                cepacity="1Gb"
-                price="1000"
-            />
-            <ProductListItem
-                name="iPhone XS"
-                description="This is phone"
-                type="phone"
-                cepacity="1Gb"
-                price="1000"
-            />
-            <ProductListItem
-                name="iPhone 12"
-                description="This is phone"
-                type="phone"
-                cepacity="1Gb"
-                price="1000"
-            />
-            <ProductListItem
-                name="iPhone 8 plus"
-                description="This is phone"
-                type="phone"
-                cepacity="1Gb"
-                price="1000"
-            />
+            <div className="product-list">
+                <ProductListItem
+                    name="iPhone X"
+                    description="This is phone"
+                    type="phone"
+                    cepacity="1"
+                    price="1000"
+                />
+                <ProductListItem
+                    name="iPhone XS"
+                    description="This is phone"
+                    type="phone"
+                    cepacity="2"
+                    price="2000"
+                />
+                <ProductListItem
+                    name="iPhone 12"
+                    description="This is phone"
+                    type="phone"
+                    cepacity="1"
+                    price="1000"
+                />
+                <ProductListItem
+                    name="iPhone 8 plus"
+                    description="This is phone"
+                    type="phone"
+                    cepacity="2"
+                    price="2000"
+                />
+            </div>
         </>
     )
 }
