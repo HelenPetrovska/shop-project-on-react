349c10c71e3b9dd720b2d3bcbb6d9f180288b32f
Helen <paznachka@gmail.com>
2021-02-16 20:21:06 +0200

    added product-quantity

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -19,6 +19,11 @@ const ProductListItem = ({
             <div className="product-description">{description}</div>
             <div className="product-features">Type: {type}</div>
             <div className="product-features">Cepacity: {cepacity} Gb</div>
+            <div className="product-quantity">
+                <button>-</button>
+                <input type="text" value="1" readOnly/>
+                <button>+</button>
+            </div>
             <div className="product-price">{price}$</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
