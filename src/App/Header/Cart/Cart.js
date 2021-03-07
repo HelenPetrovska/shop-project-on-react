8a84873c1b9a7727520f6d6150263334426bea47
Helen <paznachka@gmail.com>
2021-03-07 13:27:57 +0200

    add CartProductList

--- src/App/Header/Cart/Cart.js
+++ src/App/Header/Cart/Cart.js
@@ -1,27 +1,22 @@
 import React from 'react'
-import {keys} from 'lodash'
-import products, { getProductsObject } from '../../Main/Products/products'
 import { Link } from 'react-router-dom'
 import CartTotal from '../../../Components/Cart/CartTotal'
 
 import "./cart.css"
+import CartProductList from '../../../Components/Cart/CartProductList'
 
 
 const Cart = ({
     productsInCart,
-    productsObject=getProductsObject(products)
 }) => {
     return (
         <div className="cart text-center">
-            {
-                keys(productsInCart).map((productId) => (
-                    <div key = {productId}>{productsObject[productId].name}: {productsInCart[productId]}</div>
-                ))
-            }
+            <CartProductList
+                productsInCart={productsInCart}
+            />
             <CartTotal
                 productsInCart={productsInCart}
             />
-
             <Link to="/cart">Show cart</Link>
         </div>
     )
--- src/App/Main/CartPage/CartPage.js
+++ src/App/Main/CartPage/CartPage.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import {keys} from 'lodash'
-import products, { getProductsObject } from '../Products/products'
 import CartTotal from '../../../Components/Cart/CartTotal'
+import CartProductList from '../../../Components/Cart/CartProductList'
 
 const CartPage = ({
     productsInCart,
-    productsObject=getProductsObject(products)
 }) => {
     return (
         <>
             <h1 className="page-title">Cart Page</h1>
-            {
-                keys(productsInCart).map((productId) => (
-                    <div key = {productId}>{productsObject[productId].name}: {productsInCart[productId]}:{productsObject[productId].price}</div>
-                ))
-            }
+            <CartProductList
+                productsInCart={productsInCart}
+            />
             <CartTotal
                 productsInCart={productsInCart}
             />
