d8f9fb645cf377bb090f61a05d63190ec882e1a4
Helen <paznachka@gmail.com>
2021-04-20 10:54:28 +0300

    add toggleColor func

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -4,9 +4,22 @@ const Testimonials = () => {
 
     const [count, setCount] = useState(1)
 
+    const [color, setColor] = useState("green")
+
+    const toggleColor = () => {
+        setColor((value) => (
+            value === "green" ? "red" : "green"
+        ))
+    }
+
     return (
         <>
             <h1 className="page-title">Testimonials</h1>
+
+            <div>
+                color:{color}
+            </div>
+            <button onClick={() => toggleColor()}>Change color</button>
             <button onClick={() => setCount(count-1)}>-</button>
             <input type="text" value={count}/>
             <button onClick={() => setCount(count+1)}>+</button>
