8e16796292965dd371fd5aa023bdcdfe0700a6e5
HelenPetrovska <73559214+HelenPetrovska@users.noreply.github.com>
2021-08-16 13:24:33 +0300

    edited Main.js

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -6,22 +6,40 @@ import Shipping from './Shipping/Shipping'
 
 
 import ProductList from './Products/ProductList'
-import ProductPage from './ProductPage/ProductPage'
+import Testimonials from '../../Components/Testimonials/Testimonials'
 import CheckoutPage from './CheckoutPage/CheckoutPage'
 
-const Main = () => {
+const Main = ({
+    products,
+    addProductToCart,
+    removeProductFromCart,
+    productsInCart,
+    changeProductQuantity,
+}) => {
     return (
         <main className="main">
             <div className="container">
-                <div className="row-main">
+                <div className="row-main">zz
+                    <div className="aside">
+                        Filter
+                    </div>
                     <div className="main-content">
-                        <Route path="/" exact render={() =>(
-                                <ProductList/>
+                        <Route path="/" exact render={()=> (
+                            <ProductList
+                            addProductToCart={addProductToCart}
+                            />
                         )}/>
-                        <Route path="/cart" component={CartPage}/>
+                        <Route path="/cart" render={() => (
+                            <CartPage
+                            products={products}
+                            productsInCart={productsInCart}
+                            removeProductFromCart={removeProductFromCart}
+                            changeProductQuantity={changeProductQuantity}
+                            />
+                        )}/>
                         <Route path="/payment" component={Payment}/>
                         <Route path="/shipping" component={Shipping}/>
-                        <Route path="/products/:id" component={ProductPage}/>
+                        <Route path="/" exact component={Testimonials}/>
                         <Route path="/checkout" component={CheckoutPage}/>
                     </div>
                 </div>
